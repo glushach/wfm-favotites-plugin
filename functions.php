c2ddf76740e2131e013d5ac100c74277fb82d12a
Oleksandr Hlushach <oleksandr.hlushach@ristars.io>
2023-11-13 17:48:57 +0200

    add article in favorite

--- functions.php
+++ functions.php
@@ -4,7 +4,13 @@ function wfm_favotites_content($content)
 {
   if (!is_single() || !is_user_logged_in()) return $content;
   $img_src = plugins_url('/img/loader.gif', __FILE__);
-  return '<p class="wfm-favotites-link"><span class="wfm-favorites-hidden"><img src="'. $img_src .'" alt=""></span><a href="#">Добавить в Изранное</a></p>' . $content;
+
+  global $post;
+  if (wfm_is_favorites($post->ID)) {
+    return '<p class="wfm-favotites-link"><a href="#">Удалить из Изранного</a></p>' . $content;
+  }
+
+  return '<p class="wfm-favotites-link"><span class="wfm-favorites-hidden"><img src="' . $img_src . '" alt=""></span><a href="#">Добавить в Изранное</a></p>' . $content;
 }
 
 function wfm_favorites_scripts()
@@ -13,13 +19,15 @@ function wfm_favorites_scripts()
   wp_enqueue_script('wfm-favorites-scripts', plugins_url('/js/wfm-favorites-scripts.js',  __FILE__), array('jquery'), null, true);
   wp_enqueue_style('wfm-favorites-styles', plugins_url('/css/wfm-favorites-style.css',  __FILE__));
   global $post;
-  wp_localize_script( 'wfm-favorites-scripts', 'wfmFavotites',
-		array(
-			'url' => admin_url('admin-ajax.php'),
+  wp_localize_script(
+    'wfm-favorites-scripts',
+    'wfmFavotites',
+    array(
+      'url' => admin_url('admin-ajax.php'),
       'nonce' => wp_create_nonce('wfm-favorites'),
       'postId' => $post->ID
     )
-	);
+  );
 }
 
 function wp_ajax_wfm_callback()
@@ -27,6 +35,24 @@ function wp_ajax_wfm_callback()
   if (!wp_verify_nonce($_POST['security'], 'wfm-favorites')) {
     wp_die('Ошибка безопасности!');
   }
-  echo (int)$_POST['postId'];
-  wp_die('Запрос завершен');
+  $post_id = (int)$_POST['postId'];
+  $user = wp_get_current_user();
+
+  if (wfm_is_favorites($post_id)) wp_die();
+
+  if (add_user_meta($user->ID, 'wfm-favorites', $post_id)) {
+    wp_die('Добавленно');
+  }
+
+  wp_die('Ошибка добавления');
+}
+
+function wfm_is_favorites($post_id)
+{
+  $user = wp_get_current_user();
+  $favorites = get_user_meta($user->ID, 'wfm-favorites');
+  foreach ($favorites as $favorite) {
+    if ($favorite == $post_id) return true;
+  }
+  return false;
 }
