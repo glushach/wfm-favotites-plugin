66ef54756b46701539dd9322544f3b2909bc22da
Oleksandr Hlushach <oleksandr.hlushach@ristars.io>
2023-11-30 15:16:46 +0200

    delete post from admin panel

---
+++ js/wfm-favorites-admin-scripts.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(function ($) {
+
+    $('.wfm-favorites-del').click(function(e) {
+      e.preventDefault();
+      if(!confirm("Подтвердите удаление")) return false;
+      const post = $(this).data('post'),
+            parent = $(this).parent(),
+            loader = parent.next(),
+            li = $(this).closest('li');
+      $.ajax({
+        type: 'POST',
+        // url: '/wp-admin/admin-ajax.php',
+        url: ajaxurl,
+        data: {
+          security: wfmFavotites.nonce,
+          action: 'wfm_del',
+          postId: post,
+        },
+        beforeSend: function() {
+          parent.fadeOut(300, function() {
+            loader.fadeIn();
+          });
+        },
+        success: function(res) {
+          loader.fadeOut(300, function() {
+            li.html(res);
+          });
+        },
+        error: function() {
+          alert('Error');
+        }
+      });
+    });
+
+});
